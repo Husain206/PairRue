0f642e231170f98623b112a1959924a964b46be0
Husain206 <husainal518@gmail.com>
2026-09-09 20:29:38 +0300

    interner
    TODO: consider making your hashmap using fibonacci hashing

---
+++ include/lexer/interner.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+// #include "../core/hashmap.hpp"
+#include "../core//macros.hpp"
+#include "../core/types.hpp"
+#include <unordered_map>
+
+struct Interner {
+private:
+  // Hashmap<strview, ssize> pool_;
+  std::unordered_map<strview, u32> pool_;
+  vec<string> from_id_;
+
+public:
+  ssize interner(strview sv) {
+    auto found = pool_.find(sv);
+    if (found == pool_.end())
+      return found->second;
+    auto id = from_id_.size();
+    from_id_.push_back(static_cast<string>(sv));
+    pool_.emplace(sv, id);
+    return id;
+  };
+
+  [[nodiscard]] strview view(ssize id) {
+    ASSERT(id < from_id_.size());
+    return from_id_[id];
+  }
+};
